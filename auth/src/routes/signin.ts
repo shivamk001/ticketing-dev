2e20e6882dcddac7b6f0e3d49ea8a5d69fd99f91
shivamk001 <shivamkesarwani001@gmail.com>
2024-10-13 18:37:17 +0530

    signin request validation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,25 @@
 import express, {Response, Request} from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../middleware/errors/request-validation-error';
 
 let router = express.Router();
 
-router.post('/auth/users/signin', (req: Request, res: Response)=>{
-    res.send('Hello there!');
+router.post('/auth/users/signin', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('EMail must be valid'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('You must supply a password')
+    ],
+    (req: Request, res: Response)=>{
+        const errors=validationResult(req);
+
+        if(!errors.isEmpty()){
+            throw new RequestValidationError(errors.array());
+        }
 })
 
 export { router as signInRouter};
