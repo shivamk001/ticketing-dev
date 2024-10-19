ed47ea211bb1e845f4cc4372c02c26c86ca83208
shivamk001 <shivamkesarwani001@gmail.com>
2024-10-19 19:19:02 +0530

    create hooks in setup.ts, create app.ts, install dependencies

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,33 @@
+import {MongoMemoryServer} from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+// connect with mongodb memory server
+beforeAll(async ()=>{
+    mongo=await MongoMemoryServer.create();
+    const mongoUri=mongo.getUri();
+    await mongoose.connect(mongoUri, {})
+})
+
+// delete all data between each test
+beforeEach(async ()=>{
+    if(mongoose.connection.db){
+        // get all collections
+        const collections=await mongoose.connection.db.collections()
+
+        for(let collection of collections){
+            // delete all data for each collection
+            await collection.deleteMany({});
+        }
+    }
+})
+
+// stop server and disconnect
+afterAll(async ()=>{
+    if(mongo){
+        await mongo.stop();
+    }
+    // disconnect
+    await mongoose.connection.close();
+})
