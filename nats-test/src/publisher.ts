46e8f42fc45839588fedbc29812f09b642ee065c
shivamk001 <shivam.kesarwani001@gmail.com>
2024-11-20 23:14:09 +0530

    randomBytes to generate unique client id for each listener

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,6 +1,7 @@
 import nats from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
-const stan=nats.connect('ticketing', 'abc', {
+const stan=nats.connect('ticketing', randomBytes(4).toString('hex'), {
     url: "http://localhost:4222"
 })
 
