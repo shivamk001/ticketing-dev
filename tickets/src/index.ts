37e12a3ff91d2729ab7bde29a2daa673c70d183d
shivamk001 <shivam.kesarwani001@gmail.com>
2024-12-16 23:21:42 +0530

    nats env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,8 +11,21 @@ const start = async ()=>{
     if(!process.env.MONGO_URI){
         throw new Error('MONGO_URL is undefined');
     }
+    if(!process.env.NATS_CLIENT_ID){
+        throw new Error('NATS_CLIENT_ID is undefined');
+    }
+    if(!process.env.NATS_URL){
+        throw new Error('NATS_URL is undefined');
+    }
+    if(!process.env.NATS_CLUSTER_ID){
+        throw new Error('NATS_CLUSTER_ID is undefined');
+    }
     try{
-        await natsWrapper.connect('ticketing', 'laskjf', 'http://nats-srv:4222');
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID, 
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL
+        );
 
         natsWrapper.client.on('close',()=>{
             console.log('NATS connection closed!');
