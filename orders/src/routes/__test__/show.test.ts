8adc667cf32012715faec8af5ea82684a5e551a8
shivamk001 <shivam.kesarwani001@gmail.com>
2025-01-01 22:45:02 +0530

    show ticket routes and test

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,54 @@
+import request  from "supertest";
+import {app} from '../../app';
+import mongoose from "mongoose";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+
+it('fetches the order', async ()=>{
+    // create a ticket
+    const ticket=Ticket.build({
+        title: 'Concert',
+        price: 20
+    });
+    await ticket.save();
+
+    const user=global.signin();
+    // make a request to build an order with ticket
+    const { body: order}=await request(app)
+                                .post('/api/orders')
+                                .set('Cookie', user)
+                                .send({ticketId: ticket.id})
+                                .expect(201);
+    // make request to fetch the order
+    const { body: fetchedOrder}=await request(app)
+                                        .get(`/api/orders/${order.id}`)
+                                        .set('Cookie', user)
+                                        .send()
+                                        .expect(200)
+
+    expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it('returns an error if one user tries to fetch another users order', async ()=>{
+    // create a ticket
+    const ticket=Ticket.build({
+        title: 'Concert',
+        price: 20
+    });
+    await ticket.save();
+
+    const user=global.signin();
+    // make a request to build an order with ticket
+    const { body: order}=await request(app)
+                                .post('/api/orders')
+                                .set('Cookie', user)
+                                .send({ticketId: ticket.id})
+                                .expect(201);
+
+    // make request to fetch the order
+    const { body: fetchedOrder}=await request(app)
+                                        .get(`/api/orders/${order.id}`)
+                                        .set('Cookie', global.signin())
+                                        .send()
+                                        .expect(401);
+});
