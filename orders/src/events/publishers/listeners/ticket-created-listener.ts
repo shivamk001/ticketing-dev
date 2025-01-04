d89b858a6d1fb52be7e6b40a8d18aaee3176fb9d
shivamk001 <shivam.kesarwani001@gmail.com>
2025-01-04 23:10:44 +0530

    export queueGroupName

--- orders/src/events/publishers/listeners/ticket-created-listener.ts
+++ orders/src/events/publishers/listeners/ticket-created-listener.ts
@@ -1,10 +1,11 @@
 import { Message } from "node-nats-streaming";
 import { Subjects, Listener, TicketCreatedEvent } from "@shivamkesarwani001/ticketing_common";
 import { Ticket } from "../../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
     subject: Subjects.TicketCreated = Subjects.TicketCreated;
-    queueGroupName: string = 'orders-service';
+    queueGroupName: string = queueGroupName;
     onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
         
     }
