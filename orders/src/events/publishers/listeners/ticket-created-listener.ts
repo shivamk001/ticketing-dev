b38b175b5893b60958aceb8dcc08d6155992a7a7
shivamk001 <shivam.kesarwani001@gmail.com>
2025-01-05 22:33:52 +0530

    ticket save in onMessage

--- orders/src/events/publishers/listeners/ticket-created-listener.ts
+++ orders/src/events/publishers/listeners/ticket-created-listener.ts
@@ -6,7 +6,14 @@ import { queueGroupName } from "./queue-group-name";
 export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
     subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName: string = queueGroupName;
-    onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
-        
+    async onMessage(data: TicketCreatedEvent['data'], msg: Message): Promise<void> {
+        const {id, title, price}=data;
+        const ticket=Ticket.build({
+            id,title,price
+        });
+        await ticket.save();
+
+        msg.ack();
     }
+
 }
