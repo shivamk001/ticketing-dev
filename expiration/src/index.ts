78ff05762ec8cd7c26505aa72ac625793c4f1c43
shivamk001 <shivam.kesarwani001@gmail.com>
2025-02-23 23:17:53 +0530

    initial setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,34 @@
+import { natsWrapper } from './nats-wrapper';
+
+// CONNECT WITH MONGODB
+const start = async ()=>{
+    if(!process.env.NATS_CLIENT_ID){
+        throw new Error('NATS_CLIENT_ID is undefined');
+    }
+    if(!process.env.NATS_URL){
+        throw new Error('NATS_URL is undefined');
+    }
+    if(!process.env.NATS_CLUSTER_ID){
+        throw new Error('NATS_CLUSTER_ID is undefined');
+    }
+    try{
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID, 
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL
+        );
+
+        natsWrapper.client.on('close',()=>{
+            console.log('NATS connection closed!');
+            process.exit();;
+        });
+
+        process.on('SIGINT', ()=>natsWrapper.client.close());
+        process.on('SIGINT', ()=>natsWrapper.client.close());
+    }
+    catch(err){
+        console.error(err);
+    }
+}
+
+start();
