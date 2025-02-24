c61d16cddb6e632dc322e1c786d8fc09c5641ee2
shivamk001 <shivam.kesarwani001@gmail.com>
2025-02-24 23:09:19 +0530

    expirationqueue creation and adding of job to queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,11 +1,16 @@
 import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@shivamkesarwani001/ticketing_common";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     subject: Subjects.OrderCreated=Subjects.OrderCreated;
     queueGroupName: string=this.queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message): Promise<void> {
-        
+        await expirationQueue.add({
+            orderId: data.id
+        });
+
+        msg.ack();
     }
 }
