fbea8278bc6c0c4f6e985e71ad06a7942cef5d8e
shivamk001 <shivam.kesarwani001@gmail.com>
2025-03-06 22:18:26 +0530

    delay in expiration

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -7,9 +7,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     queueGroupName: string=this.queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message): Promise<void> {
+
+        const delay=new Date(data.expiresAt).getTime()-new Date().getTime();
+
         console.log('ORDER CREATED LISTENER');
         await expirationQueue.add({
             orderId: data.id
+        },{
+            delay
         });
 
         msg.ack();
