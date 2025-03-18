205a82d126558597b2f663d618a4cdc4dffddbce
shivamk001 <shivam.kesarwani001@gmail.com>
2025-03-18 21:47:23 +0530

    [ticketId].js getInitialProps useRequest

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,24 @@
-const TicketShow=()=>{
-    return <div>TicketShow</div>
+const TicketShow=({ticket})=>{
+    const { doRequest, errors}=useRequest({
+        url: '/api/orders',
+        method: 'POST',
+        body: {ticketId: ticket.id},
+        onSuccess: (order)=>console.log(order)
+    });
+    return (<div>
+        <h1>{ticket.title}</h1>
+        <h4>Price {ticket.price}</h4>
+        {errors}
+        <button onClick={doRequest} className="btn btn-primary">
+            Purchase
+        </button>
+    </div>)
+}
+
+TicketShow.getInitialProps=async (context, client)=>{
+    const {ticketId}=context.query;
+    const {data}=await client.get(`/api/tickets/${ticketId}`);
+    return {ticket: data};
 }
 
 export default TicketShow;
