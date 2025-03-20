ffc484061887bf6e6e3b83b61bc94011f60e9168
shivamk001 <shivam.kesarwani001@gmail.com>
2025-03-20 21:52:00 +0530

    queueGroupName in OrderCreatedListener

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,10 +1,11 @@
 import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@shivamkesarwani001/ticketing_common";
 import { Message } from "node-nats-streaming";
 import { expirationQueue } from "../../queues/expiration-queue";
+import { queueGroupName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     subject: Subjects.OrderCreated=Subjects.OrderCreated;
-    queueGroupName: string=this.queueGroupName;
+    queueGroupName: string=queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message): Promise<void> {
 
