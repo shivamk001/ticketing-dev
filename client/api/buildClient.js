bad93243c9c83a500d4036cf42fbb4f3f6ac53c5
shivamk001 <shivam.kesarwani001@gmail.com>
2025-03-20 23:27:38 +0530

    debug

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const buildClient=({req})=>{
-    console.log('BC', req.headers);
-    if(typeof window == 'undefined'){
 
+    if(typeof window == 'undefined'){
+        console.log('BC', req.headers);
         console.log('BC1', axios.create({
             baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
             headers: req.headers
