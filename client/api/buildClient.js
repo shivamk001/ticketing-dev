9c6c0e1f21df00ee1dab9c2b2457ab5eee658971
shivamk001 <shivam.kesarwani001@gmail.com>
2025-03-20 22:45:19 +0530

    buildClient debug

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 
 const buildClient=({req})=>{
+    console.log('BC', req.headers);
     if(typeof window == 'undefined'){
+
+        console.log('BC1', axios.create({
+            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            headers: req.headers
+        }));
         return axios.create({
                 baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
                 headers: req.headers
         })
     }
     else{
+        console.log('BC2', axios.create({
+            baseURL: '/'
+        }));
+        
         return axios.create({
             baseURL: '/'
         })
